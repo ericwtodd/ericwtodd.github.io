760d7afec5ee58d5fa3d7d3ad54dafc742fc58b2
Eric Todd <eric.w.todd@gmail.com>
2024-08-02 12:03:53 -0400

    update website

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.abstract-toggle').forEach(button => {
+      button.addEventListener('click', function() {
+        const abstractContainer = this.closest('.citation').querySelector('.abstract-container');
+        abstractContainer.classList.toggle('show');
+        this.textContent = abstractContainer.classList.contains('show') ? 'Abstract' : 'Abstract';
+        // this.textContent = abstractContainer.classList.contains('show') ? 'Abstract' : 'Abstract';
+      });
+    });
+  });
